1f2f4c82f0dc49acc739c3b8335946808f2e1409
David Kalinowski <dakalinowski@gmail.com>
2013-03-01 10:33:04 -0800

    clubber table creation test

--- php/create_table.php
+++ php/create_table.php
@@ -1,26 +1,56 @@
 <?php
-	session_start();
+	/*session_start();
 	
 	if(!session_is_registered(email)){
 		header("location:index.php");
-	}
+	}*/
 
 	require "config.php";
 	
-	//Create Table in DB
+	/* Tables list
+	$clubber = "clubber";
+	$administrator = "administrator";
+	$dj = "dj";
+	$club = "administrator";
+	$party = "party";
+	$webconnection = "webconnection";
+	$price = "price";
+	$drink = "drink";
+	$host = "host";
+	$do = "do";
+	$book = "book";
+	$play = "play";
+	*/
+	
+	
+	//Create clubber table
 	mysql_query("
-		/*Insert SQL query here*/
+		DROP TABLE IF EXISTS clubber;
+		CREATE TABLE IF NOT EXISTS clubber (
+		 id_user int NOT NULL AUTO_INCREMENT CONSTRAINT pk_user PRIMARY KEY,
+		 name VARCHAR(30),
+		 firstname VARCHAR(30),
+		 nickname VARCHAR(30),
+		 email VARCHAR(45),
+		 gender VARCHAR(30),
+		 age INT,
+		 city VARCHAR(30),
+		 kindaccount VARCHAR(30),
+		 );
 	)") or die(mysql_error());
  	
 	if ($conn)
 		{
-		echo "Table created. <br><br>";
+		echo "clubber table created. <br><br>";
 		}
 	else
 		{
-		echo "Table creation failed. <br><br><a href='index.html'>home</a>";
+		echo "clubber table creation failed. <br><br><a href='index.html'>home</a>";
 	
 	}
+	
+	/*NEXT TABLE CREATION*/
+	
 	echo "Complete.";
 	
 	echo "<br><br><a href='main.php'>home</a>";
