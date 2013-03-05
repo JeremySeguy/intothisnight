56976088e2b34a9fd58bbe036996659a19a7bfd0
David Kalinowski <dakalinowski@gmail.com>
2013-03-05 02:58:33 -0800

    grammar and email notification of signups

--- php/signup.php
+++ php/signup.php
@@ -27,5 +27,24 @@
 		echo 'ok';
 	}
 
+	$subject = "intothisnight.com signup";
+	$combined = "email: " . $email;
+
+	$error = '';
+
+	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+	$headers  =  'MIME-Version: 1.0' . "\r\n";
+	$headers .=  'Content-type: text/html; charset=utf-8' . "\r\n";
+
+	$headers .=  "From: site <".$email."> \r\n";
+	$headers .= "Reply-To: $email \r\n";
+	$headers .=	'X-Mailer: PHP/' . phpversion();
+
+	$mail_to_send_to = "info@intothisnight.com";			/*sending to*/
+	$your_feedbackmail = "noreply@intothisnight.com ";			/*sent from*/
+
+	$a = mail( $mail_to_send_to, $subject, $combined, $headers );
+
 	mysql_close($conn);
 ?>
