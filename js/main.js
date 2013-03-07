31b0aeffb0cf9a660532927cd1cc082427066013
Félix Bayart <felix.bayart@wanadoo.fr>
2013-03-07 09:14:55 +0100

    Reorder the files to fit the boilerplate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-
+$(document).ready(function(){
+	$("#signup").submit(function(){
+	var email = $('#signup [name="email"]').val();
+	
+	var str = $(this).serialize();
+	$.ajax({
+		type: "POST",
+		url: "php/signup.php",
+		data: str,
+		success: function(result){
+			if (result=='ok'){
+				document.getElementById("email").innerHTML='';
+				alert("Success! Will email you when ITN is ready.");
+			}
+			else{
+				alert(result);
+			}
+		}
+	});
+	
+	document.getElementById("email").onfocus=function(){
+		document.getElementById("email").innerHTML=''
+	};
+	return false;	
+	});
+});
